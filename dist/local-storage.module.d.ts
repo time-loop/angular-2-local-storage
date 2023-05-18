5469bcda91994ea30e0530694e61bdaac5373db2
Matt Lewis <hello@mattlewis.me>
2023-05-18 16:58:50 +0100

    chore: ignore dist directory

--- dist/local-storage.module.d.ts
+++
@@ -1,9 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { ILocalStorageServiceConfig } from './local-storage.config.interface';
-import * as i0 from "@angular/core";
-export declare class LocalStorageModule {
-    static forRoot(userConfig?: ILocalStorageServiceConfig): ModuleWithProviders<LocalStorageModule>;
-    static ɵfac: i0.ɵɵFactoryDeclaration<LocalStorageModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<LocalStorageModule, never, never, never>;
-    static ɵinj: i0.ɵɵInjectorDeclaration<LocalStorageModule>;
-}
